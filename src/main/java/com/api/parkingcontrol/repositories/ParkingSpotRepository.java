82d1a8c5678cbae6ad1455c9335772aaf6f0fa0d
Jhonatan PC-CASA <edisonjhonatan@gmail.com>
2023-04-07 20:16:02 -0300

    Criando o Repository para ParkingSpot

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    // JPA nos fornece métodos prontos
+    // A anotation @repository é para transações do BD do spring
+
+
+
+
+}
